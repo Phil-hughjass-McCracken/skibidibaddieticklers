249ec194a4302db8a86c9f1ed6cc72fb5f2d184d
Phil-hughjass-McCracken <phil40132@gmail.com>
2025-10-14 21:29:39 -0400

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,1 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import PasswordGate from "./components/PasswordGate";
+import LoginPage from "./pages/LoginPage";
+import BoardPage from "./pages/BoardPage";
 
+export const router = createBrowserRouter([
+  { path: "/", element: <PasswordGate /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/board", element: <BoardPage /> },
+]);
