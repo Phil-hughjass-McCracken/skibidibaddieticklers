db550bc25e425117342b8e3756951b6e4ec50f88
Phil-hughjass-McCracken <phil40132@gmail.com>
2025-10-15 07:16:45 -0400

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-600 to-pink-600 text-white">
+      <h1 className="text-5xl font-bold mb-4">Tailwind is working 😎</h1>
+      <p className="text-lg">Skibidi Baddie Ticklers Online!</p>
+    </div>
+  );
+}
 
