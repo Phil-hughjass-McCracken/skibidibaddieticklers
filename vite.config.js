ae462b4ec369853fca55314f69b9c5a21fbaf19e
Phil-hughjass-McCracken <phil40132@gmail.com>
2025-10-15 15:48:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: '.',           // ✅ Make sure this points to the folder with index.html
+  build: {
+    outDir: 'dist'
+  }
+})
