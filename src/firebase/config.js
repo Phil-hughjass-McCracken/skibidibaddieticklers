442987fbd8cd214a74274630f501875fc69b4182
Phil-hughjass-McCracken <phil40132@gmail.com>
2025-10-14 21:27:31 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDZeV25Mg-Uy66_BQ37PMhto216R2QZGb4",
+  authDomain: "skibidibaddieticklers.firebaseapp.com",
+  projectId: "skibidibaddieticklers",
+  storageBucket: "skibidibaddieticklers.firebasestorage.app",
+  messagingSenderId: "144680199751",
+  appId: "1:144680199751:web:0114227f04fd3c31a53362",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
